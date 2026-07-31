e2cb340be56314bb2751a31cfe352d71383d69d9
Inderjit Virdi <inderjit.virdi@digital.justice.gov.uk>
2026-07-31 14:37:39 +0100

    HAAR-5717: Use new openhtmltopdf based renderer for generation of pdf reports

--- hmpps-subject-access-request-test-support/src/main/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequest/PdfRenderer.kt
+++ hmpps-subject-access-request-test-support/src/main/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequest/PdfRenderer.kt
@@ -1,54 +1,24 @@
 package uk.gov.justice.digital.hmpps.subjectaccessrequest
 
-import com.itextpdf.html2pdf.HtmlConverter
-import com.itextpdf.html2pdf.attach.impl.layout.HtmlPageBreak
 import com.itextpdf.io.font.constants.StandardFonts
 import com.itextpdf.kernel.font.PdfFont
 import com.itextpdf.kernel.font.PdfFontFactory
 import com.itextpdf.kernel.pdf.PdfDocument
-import com.itextpdf.kernel.pdf.PdfWriter
 import com.itextpdf.kernel.pdf.event.AbstractPdfDocumentEvent
 import com.itextpdf.kernel.pdf.event.AbstractPdfDocumentEventHandler
 import com.itextpdf.kernel.pdf.event.PdfDocumentEvent
 import com.itextpdf.layout.Canvas
 import com.itextpdf.layout.Document
-import com.itextpdf.layout.element.AreaBreak
-import com.itextpdf.layout.element.IBlockElement
-import com.itextpdf.layout.element.Image
 import com.itextpdf.layout.element.Paragraph
 import com.itextpdf.layout.element.Text
-import com.itextpdf.layout.properties.AreaBreakType
 import com.itextpdf.layout.properties.TextAlignment
-import uk.gov.justice.digital.hmpps.subjectaccessrequest.exception.SubjectAccessRequestException
 import java.io.ByteArrayOutputStream
 
-class PdfRenderer {
+interface PdfRenderer {
 
-  fun renderSubjectAccessRequestPdf(serviceHtml: String, prn: String?, crn: String?): ByteArrayOutputStream {
-    val bodyOutputStream = ByteArrayOutputStream()
-    PdfDocument(PdfWriter(bodyOutputStream)).use { pdfDocument ->
-      val document = Document(pdfDocument)
-      document.setMargins(50F, 35F, 70F, 35F)
-      pdfDocument.addSubjectAccessRequestCustomHandler(document, "Test Subject", prn, crn)
-      document.addServiceData(serviceHtml)
-    }
-    return bodyOutputStream
-  }
+  fun renderSubjectAccessRequestPdf(serviceHtml: String, prn: String?, crn: String?): ByteArrayOutputStream
 
-  private fun Document.addServiceData(serviceHtml: String) {
-    HtmlConverter.convertToElements(serviceHtml).forEach { element ->
-      when (element) {
-        is IBlockElement -> this.add(element)
-        is Image -> this.add(element)
-        is HtmlPageBreak -> this.add(AreaBreak(AreaBreakType.NEXT_PAGE))
-        else -> {
-          throw SubjectAccessRequestException("Unsupported element type found ${element.javaClass}")
-        }
-      }
-    }
-  }
-
-  private fun PdfDocument.addSubjectAccessRequestCustomHandler(document: Document, subjectName: String, prn: String?, crn: String?) {
+  fun PdfDocument.addSubjectAccessRequestCustomHandler(document: Document, subjectName: String, prn: String?, crn: String?) {
     this.addEventHandler(PdfDocumentEvent.END_PAGE, CustomHeaderEventHandler(document, subjectName, prn, crn))
   }
 }
