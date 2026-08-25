63c46874c213531e4d29cf7f5b639d3bfed08bcb
simon.mitchell <simon.mitchell1@justice.gov.uk>
2026-08-25 12:17:19 +0100

    HAAR-5967: ⬆️ update to spring-boot-plugin-11.0.6

--- test-app/build.gradle.kts
+++ test-app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.5.7"
-  kotlin("plugin.spring") version "2.4.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.6"
+  kotlin("plugin.spring") version "2.4.10"
   kotlin("jvm") version "2.3.10"
 }
 
@@ -9,7 +9,7 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.5.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:3.0.0")
   implementation("org.springframework.boot:spring-boot-starter-webclient")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
